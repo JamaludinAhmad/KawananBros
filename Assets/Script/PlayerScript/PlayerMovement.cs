d550d4b7bae161509908ce89ff25fed54b27e416
Aguh18 <114033670+Aguh18@users.noreply.github.com>
2023-02-21 10:49:39 +0700

    tambahan animasi dan perbaikan characjter movement

--- Assets/Script/PlayerScript/PlayerMovement.cs
+++ Assets/Script/PlayerScript/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
     
     Rigidbody2D rb ;
+    Animator anim;
     // kecepatan dan daya lompat
     public float PlayerSpeed, PlayerJumpforce ,rightleft;
     //Cek diatas tanah atau tidak
-    [SerializeField] private bool IsGrounded;
+    public Transform ground;
+    public float radius;
+    public  LayerMask whatisground;
+    //untuk animasi
+    private string jumpAnimation ="jump", idleAnimation = "idle", runAnimation = "run";
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,36 +28,66 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+      
         Jump();
+
         
+    }
+    private void FixedUpdate() {
+          Move();
+
     }
 
     private void Move(){
         rightleft = Input.GetAxisRaw("Horizontal");
 
         
+
+        
         rb.velocity = new Vector2(rightleft * PlayerSpeed, rb.velocity.y);
+
+        if (rightleft != 0)
+        {
+            anim.SetTrigger(runAnimation);
+            
+        }
+        else
+        {
+            anim.SetTrigger(idleAnimation);
+        }
         if (rightleft > 0 ){
             transform.eulerAngles = Vector2.zero;
-            
+
         }
         else if( rightleft < 0 ) {
             transform.eulerAngles = Vector2.up * 180;
+         
            
         }
+      
     }
 
     void Jump(){
-        if ( Input.GetKeyDown(KeyCode.Space)  || Input.GetKeyDown(KeyCode.W) ){
+        if ( Input.GetKeyDown(KeyCode.Space) && IsGrounded()  || Input.GetKeyDown(KeyCode.W) && IsGrounded() ){
+            anim.SetTrigger(jumpAnimation);
             rb.velocity = Vector2.up * PlayerJumpforce;
         }
      
         }
+
+         bool IsGrounded(){
+            return Physics2D.OverlapCircle(ground.position, radius, whatisground);
+            
+        }
+        private void OnDrawGizmos() {
+            Gizmos.DrawWireSphere(ground.position,radius);
+        }
     }
+
 
